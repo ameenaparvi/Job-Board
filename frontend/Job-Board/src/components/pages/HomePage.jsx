206e1e9eacb0561c423023d10460cf8d67a45405
MariaJoby <mariajobypunnassery@gmail.comm>
2025-06-16 17:49:49 +0530

    Created SearchJobs

--- frontend/Job-Board/src/components/pages/HomePage.jsx
+++ frontend/Job-Board/src/components/pages/HomePage.jsx
@@ -1,16 +1,36 @@
-import { Box, Card, CardActions, CardContent, Grid, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Box, Button, Card, CardActions, CardContent, Grid, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
 export const HomePage = () => {  
     const[jobs,setJob]=useState([]);
+    useEffect(() => {
+        setJob([
+          {
+            _id: '1',
+            title: 'Frontend Developer',
+            company: 'TechCorp',
+            location: 'Remote',
+            role: 'React + MUI',
+          },
+          {
+            _id: '2',
+            title: 'Backend Developer',
+            company: 'CodeBase',
+            location: 'Bangalore',
+            role: 'Node.js + MongoDB',
+          },
+        ]);
+      }, []);
+    
 
 
   return (
     <div>
         <Box>
             <br/> <br/>
-            <Typography variant="h5" gutterBottom>Available Job Listings</Typography>
+            <Typography variant="h4" gutterBottom>Available Job Listings</Typography><br/><br/>
             <Grid container spacing={3}>
+                
                 {jobs.map((job)=>{
                     return(
                         <Grid item xs={12} sm={6} md={4}>
